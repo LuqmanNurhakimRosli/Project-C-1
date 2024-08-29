fda4ef9bba0dc1ee0595dbdde34877ae0b25f877
Luqman Nurhakim <149885775+LuqmanNurhakimRosli@users.noreply.github.com>
2024-08-29 23:13:29 +0800

    new file sum

---
+++ SumInteger.c
@@ -0,0 +1,14 @@
+//operation of sum between two integers
+#include <stdio.h>
+
+int main() {
+    int n1, n2, sum;
+
+    printf("Input put 2 numbers:- ");
+    scanf("%d, %d", n1,n2);
+
+    sum = n1 + n2;
+    
+    printf("Result is: %d\n", sum);
+    return 0;
+}
