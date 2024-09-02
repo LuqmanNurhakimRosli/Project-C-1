341aab0136062254f162959e9fd592657058f1e0
Luqman Nurhakim <149885775+LuqmanNurhakimRosli@users.noreply.github.com>
2024-09-02 19:01:33 +0800

    variable file

---
+++ VariableinC.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+
+    int x; //variable
+    x = 100; //initialization
+    int y = 90; //variable + initialization
+
+    int age = 20; //intitger (%d)
+    float average = 3.5; //float of points numbers (%f)
+    char grade = 'A'; //single character (%c)
+    char name[] = "Luqman"; //array of characters (%s)
+
+    printf("My name is %s.\n", name);
+    printf ("Im age right now is %d.\n", age);
+    printf("Average of my cgpa is %f.\n", average);
+    printf("My favourite subject i got %c." grade);
+
+    return 0;
+
+}
