500cfffb71e9740d0a48a1de57d37efacf327bcc
Luqman Nurhakim <149885775+LuqmanNurhakimRosli@users.noreply.github.com>
2024-09-03 13:21:04 +0800

    Add files via upload

---
+++ FormatSpecify.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main () {
+
+    // %c = character
+    //%s = string (array of characters)
+    //%f = float
+    //%1f = double
+    //%d = integer
+
+    //%.1 = decimal precision
+    //contoh item 1 = 10.51,
+    // printf("RM %.1f\n",item1); = RM10.5, kalau nak 2 decimal jadi %.2f
+
+    //%1 = minimum field widtsh
+}
