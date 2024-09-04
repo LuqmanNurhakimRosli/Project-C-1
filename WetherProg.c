51f0b3647a7ae9589115334773ec1aae0c9f5013
Luqman Nurhakim <149885775+LuqmanNurhakimRosli@users.noreply.github.com>
2024-09-04 23:10:32 +0800

    wether

---
+++ WetherProg.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main () {
+
+    float temp = 30;
+    bool hot ;
+    
+    if (0 <= temp && temp <= 30 && hot == 0)  {
+        printf("Wether is good");
+    }
+    
+    else {
+        printf("Not good!");
+    }
+    
+    
+    
+    
+    return 0;
+}
