af5d485ff143d034a81b4d170b121c166d916561
Luqman Nurhakim <149885775+LuqmanNurhakimRosli@users.noreply.github.com>
2024-09-06 09:52:47 +0800

    function with loop

---
+++ function.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+//functionn
+void birthday() {
+    int i;
+    //for loop
+    for(i = 0; i <=2; i++){
+        printf("Happy birthday!\n");
+    }
+
+    //while loop
+    while ( i <= 2){
+        printf ("Happy Birthday!\n");
+        i++;
+    }
+
+
+
+}
+
+int main () {
+    birthday();
+
+}
