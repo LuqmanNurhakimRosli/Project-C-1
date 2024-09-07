dbd89c5963e31c7c61671e5d2426085d723803a1
Luqman Nurhakim <149885775+LuqmanNurhakimRosli@users.noreply.github.com>
2024-09-07 12:47:06 +0800

    protoFunction

---
+++ prototype_func.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+//prototype function identity
+void identity( char [] , int);
+
+int main() {
+
+    char name [] = "Luqman";
+    int age = 20;
+
+    identity(name, age) ;
+
+
+    return 0;
+}
+
+void identity(char name[] , int age){
+
+    printf("Hello %s\n", name);
+    printf("You are %d\n", age);
+
+
+}
+
+// function prototype
+// function declaration w/o body , before main()
+// Ensure that calls to function are made with correct arguement
+
+// IMPORTANT NOTES
+// Many C compiler not check for parameter matching
+// Missing arguement will result in unexpected behaviour
+// A function prototype causes the compiler to flag an error if arguement are misssing
