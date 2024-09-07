c78061cfa65d7218488c7b3c89dba19d2d770bf3
Luqman Nurhakim <149885775+LuqmanNurhakimRosli@users.noreply.github.com>
2024-09-07 12:48:27 +0800

    Update

--- prototype_func.c
+++ prototype_func.c
@@ -30,3 +30,8 @@ void identity(char name[] , int age){
 // Many C compiler not check for parameter matching
 // Missing arguement will result in unexpected behaviour
 // A function prototype causes the compiler to flag an error if arguement are misssing
+
+// ADVANTAGES
+// 1. Easier to navigate a program w/ main() at top
+// 2. HElp with debugging
+// 3. Commonly used in header files
