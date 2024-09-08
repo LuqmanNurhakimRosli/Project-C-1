32b057714400ca0cc3cf3e3a24b9dc306554cb21
Luqman Nurhakim <149885775+LuqmanNurhakimRosli@users.noreply.github.com>
2024-09-08 12:50:22 +0800

    update

--- while_loop.c
+++ while_loop.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 #include <string.h>
 
-//untuk input char name, guna fgets(); name[];, instead scanf();
+//untuk input char name, guna fgets(); name[];, instead scanf()
+//fgets() is function used to read string from input stream
+//stdin is the standard input stream, typically the keyboard.
+//When the user types their name and presses Enter, fgets() reads the input, including the newline character ('\n') added when the user hits Enter.
+
+//strlen(name) calculates the length of the string stored in the name array.
+//strlen(name) - 1 gives the index of the last character in the string, which is the newline character ('\n').
+// name[strlen(name) - 1] = '\0'; replaces this newline character with the null terminator ('\0').
+//This step effectively removes the newline character from the input, so that when the name is printed later, it does not include an unwanted newline at the end.
 
 int main() {
 
