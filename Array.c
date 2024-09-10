0784adabc6e0f2867c973e867bef668027cddc0c
Luqman Nurhakim <149885775+LuqmanNurhakimRosli@users.noreply.github.com>
2024-09-10 14:23:42 +0800

    Create Array.c

---
+++ Array.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+//array = a data structure that can store many values of the same data type
+int main() {
+    double prices[] = {5.0, 4.0, 3.0, 2.0, 1.0};
+    char names[] [10] = {"Luqman", "Nurhakim", "Bin", "Rosli"};
+    
+   
+
+
+  for (int i = 0; i < && i   ; i++)
+  {
+      printf("RM%.2lf \n", prices[i]);
+      printf("Hello %s ", names[i]);
+
+  }
+
+    return 0;
+}
